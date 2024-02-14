4907f1ad4016f333561d1b14cbffcd3079b72dba
Junnaid Iqbal <junnaid966@gmail.com>
2024-02-14 14:04:09 +0200

    14/2/2024 Commit - Python Practice Tasks

---
+++ Python Projects/RockPaperScissor.py
@@ -0,0 +1,51 @@
+#Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of 
+#congratulations to the winner, and ask if the players want to start a new game)
+
+#Remember the rules:
+
+#Rock beats scissors
+#Scissors beats paper
+#Paper beats rock
+
+
+import sys
+
+
+user1 = input('Enter 1st username: ')
+user2 = input('Enter 2nd username: ')
+
+userans1 = input("%s, what do you choose - Rock/Paper/Scissor: " %user1)
+userans2 = input("%s, what do you choose - Rock/Paper/Scissor: " %user2)
+
+def compare(u1,u2):
+
+    if u1 == u2:
+        return('It is a tie')
+
+    elif u1 == 'rock':
+        if u2 == 'scissor':
+            return('Rock wins')
+        else:
+            return('Paper Wins')
+        
+    elif u1 == 'scissor':
+        if u2 == 'paper':
+            return('Scissor Wins!')
+        else:
+            return('Rock Wins!')
+        
+    elif u1 == 'paper':
+        if u2 == 'rock':
+            return('Paper Wins!')
+        else:
+            return('Scissor Wins!')
+        
+    else:
+        return('Invalid input')
+        sys.exist()
+
+print(compare(userans1,userans2))
+        
+
+ 
+    
---
+++ Python Projects/StringList.py
@@ -0,0 +1,13 @@
+#Ask the user for a string and print out whether this string is a palindrome or not. 
+#(A palindrome is a string that reads the same forwards and backwards.)
+
+
+word = str(input('Enter the word: '))
+reverse = word[::-1]
+
+print(word+' in backward is: ' +reverse)
+
+if reverse == word:
+    print('This word is palindrome: ' + word)
+else:
+    print('This word is not palindrome: ' + word)
