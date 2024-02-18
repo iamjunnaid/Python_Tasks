1e88a4bbfdbe2bd78720697ff2abdaab263c1546
Junnaid Iqbal <junnaid966@gmail.com>
2024-02-18 10:12:13 +0200

    18/2/2024 Commit - Python Practice Tasks

---
+++ Python Projects/ListEnd.py
@@ -0,0 +1,10 @@
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first 
+# and last elements of the given list. For practice, write this code inside a function.
+
+
+
+def listend(number):
+    return print(number[0], number[len(number)-1])
+
+a = [5, 10, 15, 20, 25]
+listend(a)
