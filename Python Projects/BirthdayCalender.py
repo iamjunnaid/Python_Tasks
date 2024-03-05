c95be67034366f844ac0385ef2e4014abde5008d
Junnaid Iqbal <junnaid966@gmail.com>
2024-03-05 16:07:53 +0200

    05/3/2024 Commit - Python Practice Tasks

---
+++ Python Projects/BirthdayCalender.py
@@ -0,0 +1,23 @@
+'''
+For this exercise, we will keep track of when our friend’s birthdays are, and be able to find that information based on their name. 
+Create a dictionary (in your file) of names and birthdays. When you run your 
+program it should ask the user to enter a name, and return the birthday of that person back to them. 
+'''
+
+birthdays={
+    'Albert Einstein':'03/14/1879',
+    'Benjamin Franklin':'01/17/1706',
+    'Ada Lovelace':'12/10/1815',
+    'Donald Trump':'06/14/1946',
+    'Rowan Atkinson':'01/6/1955',
+}
+print('Welcome to Birthday Calender. We know the birthdays of: ')
+for name in birthdays:
+    print(name)
+
+print('Who\'s birthday do you want to know: ')
+name = input()
+if name in birthdays:
+    print('{}\'s birthday is {}.'.format(name,birthdays[name]))
+else:
+    print('Sadly we don\'t have the birthday information of {}.'.format(name))
